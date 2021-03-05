3edef7a8cb26f86f30faa76307ba229e718a62d0
vhernandezc <vhernandezc7@miumg.edu.gt>
2021-03-05 16:33:22 -0600

    PROGRAMAS

---
+++ puerba examen/p/p/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace p
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+             label.Text = "piensa en un numero";
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            label.Text = "multiplica el numero que pensaste por 2";
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            label.Text = "ahora sumale 8";
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+            label.Text = "el resultado multiplicalo por 5";
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+            resultado.IsEnabled = true;
+            label.Text = "ingrese el resultado";
+
+        }
+
+        private void Button_Clicked_5(object sender, EventArgs e)
+        {
+            string num = (resultado.Text);
+            num = num.Remove(num.Length - 1);
+            int a, b;
+            b = int.Parse(num);
+            a = b - 4;
+            calcular.Text = a.ToString();
+
+        }
+        
+        
+    }
+}
---
+++ puerba examen/p2/p2/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace p2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            label.Text = "anota el día en que naciste";
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            label.Text = "multiplicalo por 20";
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            label.Text = "ahora sumale 73";
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+            label.Text = "ahora multiplicalo por 5";
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+            label.Text = "sumale el numero de mes de tu cumpleaños";
+        }
+
+        private void Button_Clicked_5(object sender, EventArgs e)
+        {
+            label.Text = "ingresa el resultado";
+        }
+
+        private void Button_Clicked_6(object sender, EventArgs e)
+        {
+            string num = (resultado.Text);
+            int a, b;
+            b = int.Parse(num);
+            a = b - 365;
+            calcular.Text = a.ToString("##/##");
+        }
+    }
+}
